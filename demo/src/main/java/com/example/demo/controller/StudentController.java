441b8c0d995bde730c1a3f6719d18bf0cc8caaa8
piumal302 <139644276+piumal302@users.noreply.github.com>
2025-03-28 09:16:33 +0530

    Update StudentController.java

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,15 @@
 package com.example.demo.controller;
 
-public class StudentController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+@GetMapping("/name")
+	public String myMessage (){
+	return "MY name is Piuymal";
+
+}
 }
